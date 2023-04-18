d2b8f4c4779c333c5e3c803a2c61e12baa23185f
rory jackson <roryjamesjackson@hotmail.com>
2023-04-18 10:26:28 +0100

    build basic html for about

---
+++ src/components/projects/projects.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './projects.css';
+
+function Projects() {
+  return (
+      <div className='projects-container'>
+        <div className='standard-block'>
+          <h1>Im Rory, a full stack developer doing shit</h1>
+          <p>Ive been designing and implementing small apps for companies etc etc etc</p>
+          <ul>
+            <li>Github</li>
+            <li>Instagram</li>
+            <li>linkedIn</li>
+            <li>Contact me</li>
+          </ul>
+        </div>
+      </div>
+    )
+}
+
+
+export default Projects;
