cae93b8ad4eab7d192ab62ff2be2ac0e91cacf9f
rory jackson <roryjamesjackson@hotmail.com>
2023-04-19 11:58:05 +0100

    updated the projects description

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -4,23 +4,26 @@ import './projects.css';
 function Projects() {
 
   let projects = [
-    {title: "Zoe and Cam 2023", tag: "A react app built to handle various functions such as RSVP", img_url: "https://res.cloudinary.com/dfipoufmj/image/upload/v1669048522/zoe_header_pic_2_nsu9ml.jpg", link_url: "https://zoeandcam2023.com/"},
-    {title: "FaceDec", tag: "Full stack rails/react application that interacts with multiple apis to give information about a celebrity", img_url: "https://res.cloudinary.com/dfipoufmj/image/upload/v1681827781/pexels-alexander-grey-1209843_svkaow.jpg", link_url: ""},
-    {title: "Accounting", tag: "An express/react full stack application, that handles deposits and withdrawals to a bank account using an external accounting api", img_url: "https://res.cloudinary.com/dfipoufmj/image/upload/v1681827428/pexels-olia-danilevich-5466820_fovhjh.jpg", link_url: ""},
-    {title: "Giphy", tag: "My first project getting used to using APIs. I impleneted a simple app where you can search through and find funny gifs", img_url: "https://res.cloudinary.com/dfipoufmj/image/upload/v1681827477/pexels-mark-glancy-1564506_kpjyoq.jpg", link_url: ""},
-    {title: "Blog", tag: "My first full stack project, this implements a blog style app where all crud actions are available, as well as displaying on a map service for easier use", img_url: "https://res.cloudinary.com/dfipoufmj/image/upload/v1681827598/pexels-miguel-a%CC%81-padrin%CC%83a%CC%81n-1111370_q8bkvh.jpg", link_url: "https://basicblogtemplate.herokuapp.com/"},
-    {title: "roryjjackson.com", tag: "My personal portfolio code base", img_url: "https://res.cloudinary.com/dfipoufmj/image/upload/v1669281725/pexels-kelly-13766418_etqld7.jpg", link_url: ""},
+    {title: "FaceDec", tag: "Full stack rails/react application that takes an uploaded image from a user, identifies any celebrities in the image and finally displays any information found about them", img_url: "https://res.cloudinary.com/dfipoufmj/image/upload/v1681827781/pexels-alexander-grey-1209843_svkaow.jpg", link_url: "", codebase: ""},
+    {title: "Accounting", tag: "An express/react full stack application, that handles deposits and withdrawals to a virtual bank account and stores the transaction history", img_url: "https://res.cloudinary.com/dfipoufmj/image/upload/v1681827428/pexels-olia-danilevich-5466820_fovhjh.jpg", link_url: "", codebase: ""},
+    {title: "Giphy", tag: "This app handles a user input search claim and connects with an exteranl api displaying all relevant gifs", img_url: "https://res.cloudinary.com/dfipoufmj/image/upload/v1681827477/pexels-mark-glancy-1564506_kpjyoq.jpg", link_url: "", codebase: ""},
+    {title: "Zoe and Cam 2023", tag: "A front-end app with integrated services to make organizing a wedding a breeze", img_url: "https://res.cloudinary.com/dfipoufmj/image/upload/v1669048522/zoe_header_pic_2_nsu9ml.jpg", link_url: "https://zoeandcam2023.com/", codebase: ""},
+    {title: "Blog", tag: "My first full stack project. It's a blog style application that has an intergrated map to display and find posts easily, as well as allowing users to interact with other users", img_url: "https://res.cloudinary.com/dfipoufmj/image/upload/v1681827598/pexels-miguel-a%CC%81-padrin%CC%83a%CC%81n-1111370_q8bkvh.jpg", link_url: "https://basicblogtemplate.herokuapp.com/", codebase: "https://github.com/roryjjackson/rails-blog"},
+    {title: "Personal Portfolio", tag: "The source code on github for the site you're currently browsing", img_url: "https://res.cloudinary.com/dfipoufmj/image/upload/v1669281725/pexels-kelly-13766418_etqld7.jpg", link_url: "https://github.com/roryjjackson/personalportfoliov2", codebase: ""},
   ]
   return (
       <div className='projects-container' id="projects">
           {projects.map(project => {
             return(
-              <a href={project.link_url} target="_blank" rel="noopener noreferrer" className='projects-a'>
-                <div className='standard-block projects' style={{backgroundImage: `linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,00)),url(${project.img_url})`}}>
+              <div className='standard-block projects' style={{backgroundImage: `linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,00)),url(${project.img_url})`}}>
+                <a href={project.link_url} target="_blank" rel="noopener noreferrer" className='projects-a'>
                   <h2>{project.title}</h2>
                   <p>{project.tag}</p>
-                </div>
-              </a>
+                </a>
+                <button className='codebase-button' >
+                  <a href={project.codebase} target="_blank" rel="noopener noreferrer">Source Code</a>
+                </button>
+              </div>
             )
           })}
       </div>
