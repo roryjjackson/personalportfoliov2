c90d05b7627206c15e9dbba6c567cf420cfde133
rory jackson <roryjamesjackson@hotmail.com>
2023-06-13 10:27:22 +0100

    changed urls and images for projects

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,7 +10,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 2500);
+    setTimeout(() => setLoading(false), 700);
   }, []);
 
   return (
